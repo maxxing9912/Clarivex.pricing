be7ee6f38114399a4b27995b8f45b79f0b4c61d8
maxxing9912 <139047507+maxxing9912@users.noreply.github.com>
2025-06-10 22:31:39 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    DISCORD_CLIENT_ID: "1370487453562962101",
+    DISCORD_CLIENT_SECRET: "r-4-RqKenXJET83zteNpBkbVFqan9BCy",
+    NEXTAUTH_URL: "http://localhost:3000/",
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: "pk_test_51RTquJGjdxMu1OKbOXLmErQAVDy7416CN8lraiP2YsPNH4ufZUFgNiuuQkNRNtnqyyc8hXG02rYYnNVJEm5uH1Re00pOIg6RO2",
+  },
+};
+
+module.exports = nextConfig;
